72b7eafbe09b53268135fd3cb6f8a49e35aa81fa
Deya Genao <deyagenao@gmail.com>
2019-09-22 11:46:56 -0400

    updated html and javascript to account for the chat box

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,12 +10,20 @@ var firebaseConfig = {
   };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-var database = firebase.database();
 
 // Global Variables
+var database = firebase.database();
+var chatData = database.ref("/chat");
+var playersRef = database.ref("players");
+var currentTurnRef = database.ref("turn");
 var username = "Guest";
-var player1Connected = false;
-var player2Connected = false;
+var currentPlayers = null;
+var currentTurn = null;
+var playerNum = false;
+var playerOneExists = false;
+var playerTwoExists = false;
+var playerOneData = null;
+var playerTwoData = null;
 
 // USERNAME LISTENERS
 // Click event for the submit name button 
@@ -33,23 +41,86 @@ $("#enter-name").keypress(function(event){
     }
 });
 
-// Global Functions 
+// Chat Listeners
+// Chat send button listener, grabs input and pushes to firebase (Firebase's push automatically creates a unique key)
+$("#chat-send").click(function(){
+    if ($("chat-input").val() !== ""){
+        var message = $("#chat-input").val();
+
+        chatData.push({
+            name: username,
+            message: message,
+            time: firebase.database.ServerValue.TIMESTAMP,
+            idNum: playerNum
+        });
+
+        $("#chat-input").val("");
+    }
+});
+
+//Chat input listener
+$("#chat-input").keypress(function(event){
+    if (event.which === 13 && $("#chat-input").val() !== ""){
+        var message = $("#chat-input").val();
+
+        chatData.push({
+            name: username, 
+            message: message,
+            time: firebase.database.ServerValue.TIMESTAMP,
+            idNum: playerNum
+        });
+
+        $("#chat-input").val("");
+    }
+});
+
+// Update chat on screen when new message detected - ordered by 'time' value
+chatData.orderbyChild("time").on("child_added", function(snapshot){
+    $("#chat-messages").append(
+        $("<p>").addClass("player-" +snapshot.val().idNum),
+        $("<span>").text(snapshot.val().name + ": " + snapshot.val().message)
+    );
+
+    //Keeps div scrolled to bottom on each update 
+    $("#chat-messages").scrollTop($("#chat-messages")[0].scrollHeight);
+});
+
+// Click event for dynamically added <li> elements, or game choices
+$(document).on("click", "li", function(){
+    console.log("click");
+
+    // Grabs text from li choice
+    var clickChoice = $(this).text();
+    console.log(playerRef);
+
+    // Sets the choice in the current player firebase 
+    playerRef.child("choice").set(clickChoice);
+
+    //User has chosen, so removes choices and displays that they chose 
+    $("#player" + playerNum + "ul").empty();
+    $("#player" + playerNum+ "chosen").text(clickChoice);
+
+    // Increments turn. Turn goes from: 
+    // 1 - player 1 
+    // 2 - player 2
+    // 3 - determine winner
+    currentTurnRef.transaction(function(turn){
+        return turn + 1;
+    });
+});
+
+// Tracks changes in key which contains player objects 
+playersRef.on("value", function(snapshot){
+    //length of players array
+    currentPlayers = snapshot.numChildren();
+
+    //Check to see if players exist
+    playerOneExists = snapshot.child("1").exists();
+    playerTwoExists = snapshot.child("2").exists();
+})
+
+// Function for starting the game
 function startGame() {
 
 }
 
-
-
-
-
-//     if (!player1Connected){
-//     player1Name = $("#enter-name").val().trim();
-//     $("#player-1").text(player1Name)
-//     $("#game-status").empty();
-//     } else if (!player2Connected) {
-//         player2Name = $("#enter-name").val().trim();
-//         $("#player-2").text(player2Name)
-//         $("#game-status").empty();
-//     } else {
-//         $("#game-status").html("<h5>Sorry, the game is currently full. Try again soon!</h5>")
-//     }
